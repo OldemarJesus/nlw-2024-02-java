5ee9126564989015497ae27303c637e1dfd9ee31
Oldemar <oldemego@gmail.com>
2024-02-07 19:51:17 +0000

    intial commit

---
+++ src/main/java/uk/techboystore/certificationsystem/CertificationsystemApplication.java
@@ -0,0 +1,13 @@
+package uk.techboystore.certificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/techboystore/certificationsystem/CertificationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.techboystore.certificationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
